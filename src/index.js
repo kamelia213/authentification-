7795c43e5a0ae355aedcb78f1da5044736435a24
kamelia213 <149701133+kamelia213@users.noreply.github.com>
2026-01-13 00:46:02 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ const { connectDB } = require("./config");
 const User = require("./models/User");
 
 const app = express();
-// convert data into json format
+
 app.use(express.json());
-// Static file
+
 app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: false }));
-//use EJS as the view engine
+
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
@@ -43,7 +43,7 @@ app.get("/home", (req, res) => {
     return res.render("home", { username: req.session.user.username });
 });
 
-// Register User
+
 app.post("/signup", async (req, res) => {
 
     const data = {
@@ -51,17 +51,17 @@ app.post("/signup", async (req, res) => {
         password: req.body.password
     }
 
-    // Check if the username already exists in the database
+
     const existingUser = await User.findOne({ username: data.username });
 
     if (existingUser) {
         return res.redirect("/signup?error=Utilisateur%20deja%20existant");
     } else {
-        // Hash the password using bcrypt
-        const saltRounds = 10; // Number of salt rounds for bcrypt
+     
+        const saltRounds = 10; 
         const hashedPassword = await bcrypt.hash(data.password, saltRounds);
 
-        data.password = hashedPassword; // Replace the original password with the hashed one
+        data.password = hashedPassword;
 
         const userdata = await User.create(data);
         console.log(userdata);
@@ -70,14 +70,14 @@ app.post("/signup", async (req, res) => {
 
 });
 
-// Login user 
+ 
 app.post("/login", async (req, res) => {
     try {
         const check = await User.findOne({ username: req.body.username });
         if (!check) {
             return res.redirect("/?error=Utilisateur%20introuvable");
         }
-        // Compare the hashed password from the database with the plaintext password
+
         const isPasswordMatch = await bcrypt.compare(req.body.password, check.password);
         if (!isPasswordMatch) {
             return res.redirect("/?error=Mot%20de%20passe%20incorrect");
@@ -98,7 +98,7 @@ app.post("/logout", (req, res) => {
     });
 });
 
-// Define Port for Application
+
 const port = process.env.PORT || 5000;
 
 async function startServer() {
